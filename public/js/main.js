617b739d6aed64112134601d239771185c6257e8
Till Kemper <till.kemper@yahoo.de>
2022-04-28 18:01:29 +0200

    feat: added more sounds

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,10 @@
-const pull_request_opened_messages = ["Just opened a pull request 🧐", "Look at my juicy pull request! 🔥🔥"]
-const pull_request_merged_messages = ["Just put out a new release! 🎉", "New version released 🔥🔥", "Just made our product better 🥳"]
-const pull_request_closed_messages = ["We had to close this one 💩", "Sorry guys i made mistake 😅"]
+const pull_request_opened_messages = ["just opened a pull request 🧐", "look at him, he opened a pull request 🔥🔥"]
+const pull_request_merged_messages = ["just put out a new release! 🎉", "just released a new version released 🔥🔥", "just made our product better 🥳"]
+const pull_request_closed_messages = ["had to close this one 💩", "made mistake 😅"]
+
+const success_sounds = ["cheers"];
+const error_sounds = ["error", "sadtrombone", "emotional_damage"];
+const wow_sounds = ["wow", "tuturu"];
 
 
 class EventHandler {
@@ -11,9 +15,21 @@ class EventHandler {
         })
     }
 
-    playSuccessSound() {
-        let audio = new Audio('audio/tuturu.mp3');
-        audio.play();
+    playSound(type) {
+        switch(type) {
+            case 'success':
+                let s_audio = new Audio(`audio/${success_sounds[Math.floor(success_sounds.length * Math.random())]}.mp3`);
+                s_audio.play();
+                break;
+            case 'wow':
+                let w_audio = new Audio(`audio/${wow_sounds[Math.floor(wow_sounds.length * Math.random())]}.mp3`);
+                w_audio.play();
+                break;
+            case 'error':
+                let e_audio = new Audio(`audio/${error_sounds[Math.floor(error_sounds.length * Math.random())]}.mp3`);
+                e_audio.play();
+                break;
+        }
     }
 
     addNewEvent(event) {
@@ -26,7 +42,7 @@ class EventHandler {
                 <div class="profileInfoWrapper">
                     <div class="profileName">${event.name}</div>
                     <div class="profileUserName">@${event.username}</div>
-                    <div class="time">${event.timestamp}</div>
+                    <div class="time" data-timestamp="${event.timestamp}">${moment(event.timestamp).fromNow()}</div>
                 </div>
             </div>
             <div class="eventContent">
@@ -43,6 +59,24 @@ class EventHandler {
             this.updateAnimation();
         }, 400)
         
+        window.setTimeout(() => {
+            this.runParty();
+            switch(eventType){
+                case 'pull_request_opened':
+                    this.playSound('wow')
+                    break
+                case 'pull_request_merged':
+                    this.playSound('success')
+                    break
+                case 'pull_request_closed':
+                    this.playSound('error')
+                    break
+                default:
+                    this.playSound('success')
+                    break
+            }
+            
+        }, 1600)
     }
 
     async setupParty() {
@@ -66,7 +100,7 @@ class EventHandler {
                     <div class="profileInfoWrapper">
                         <div class="profileName">${event.name}</div>
                         <div class="profileUserName">@${event.username}</div>
-                        <div class="time">${event.timestamp}</div>
+                        <div class="time" data-timestamp="${event.timestamp}">${moment(event.timestamp).fromNow()}</div>
                     </div>
                 </div>
                 <div class="eventContent">
@@ -82,24 +116,30 @@ class EventHandler {
         })
     }
 
+    updateTime() {
+        const eventElements = document.querySelectorAll('.events');
+        eventElements.forEach((eventElement) => {
+            let timeElement = eventElement.querySelector(".time");
+            let timeStamp = timeElement.getAttribute("data-timestamp");
+            timeElement.innerHTML = moment(timeStamp).fromNow();
+        });
+    }
+
     updateAnimation() {
         this.events.forEach((event) => {
             if(event.position == -1) {
                 event.position = 0;
             } else {
                 event.position += 1;
-                console.log(event.id);
-                console.log(this.events);
                 document.querySelector(`[data-id='${event.id}']`).className = `event position-${event.position > 3 ? 'hidden' : event.position}`;
             }
         });
 
-        this.playSuccessSound();
+        this.playSound('success');
 
         window.setTimeout(() => {
             document.querySelector('.event.new').classList.add('position-0');
             document.querySelector('.event.new').classList.remove('new');
-            this.runParty()
         }, 800)
     }
 
@@ -128,6 +168,9 @@ class EventHandler {
 
     async getNewEvents() {
         const newEvents = await fetch('events.json').then(response => response.json());
+        newEvents.sort(function(a,b){
+            return new Date(a.timestamp) - new Date(b.timestamp);
+        }); //getting the oldest events
         newEvents.forEach((newEvent) => {
             if(!this.inQueue(newEvent.id) && !this.inEvents(newEvent.id)) {
                 this.queue.push(newEvent);
@@ -161,6 +204,7 @@ class EventHandler {
         }, 5000)
 
         setInterval(() => {
+            this.updateTime();
             this.resolveQueue();
         }, 4000)
     }
